048b5e95bd9a5ab4890749b4eacf73c02bf57f6f
Misboplen <misboplen@yandex.ru>
2021-08-15 16:00:23 +0300

    Divide Slice for song implemented.

--- internal/utils/divideslice.go
+++ internal/utils/divideslice.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"errors"
+
+	"github.com/ozonva/ova-song-api/internal/models"
 )
 
 func DivideSlice(s []int, batchSize int) ([][]int, error) {
@@ -14,6 +16,31 @@ func DivideSlice(s []int, batchSize int) ([][]int, error) {
 	}
 
 	result := make([][]int, divCeil(len(s), batchSize))
+
+	fullBatches := len(s) / batchSize
+	pos := 0
+	for i := 0; i < fullBatches; i++ {
+		result[i] = s[pos : pos+batchSize]
+		pos += batchSize
+	}
+
+	if pos != len(s) {
+		result[len(result)-1] = s[pos:]
+	}
+
+	return result, nil
+}
+
+func DivideSliceOfSongs(s []models.Song, batchSize int) ([][]models.Song, error) {
+	if batchSize <= 0 {
+		return nil, errors.New("batch size must be positive")
+	}
+
+	if s == nil {
+		return nil, errors.New("slice must be not nil")
+	}
+
+	result := make([][]models.Song, divCeil(len(s), batchSize))
 
 	fullBatches := len(s) / batchSize
 	pos := 0
