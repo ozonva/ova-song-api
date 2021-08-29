c1b51fd67dbce05a2683a72e69b788ef04cc7603
Misboplen <misboplen@yandex.ru>
2021-08-29 18:39:00 +0300

    GRPC service.

--- cmd/ova-song-api/main.go
+++ cmd/ova-song-api/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"net"
+
+	api "github.com/ozonva/ova-song-api/internal/api"
+	desc "github.com/ozonva/ova-song-api/pkg/ova-song-api"
+	log "github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
 
 func main() {
-	fmt.Println("Hell_0 from `ova-song-api`.")
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal().Msgf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	desc.RegisterOvaSongApiServer(s, api.NewApi())
+	if err := s.Serve(lis); err != nil {
+		log.Fatal().Msgf("failed to serve: %v", err)
+	}
+	log.Fatal().Msgf("11failed to serve: %v", err)
 }
