cb28d2c257074e6aed56e92c44829d95cc557a6c
Misboplen <misboplen@yandex.ru>
2021-09-07 01:24:16 +0300

    Add external configs.

--- cmd/ova-song-api/main.go
+++ cmd/ova-song-api/main.go
@@ -8,21 +8,21 @@ import (
 	"github.com/jmoiron/sqlx"
 	api "github.com/ozonva/ova-song-api/internal/api"
 	br "github.com/ozonva/ova-song-api/internal/broker"
+	"github.com/ozonva/ova-song-api/internal/configuration"
 	rp "github.com/ozonva/ova-song-api/internal/repo"
 	"github.com/ozonva/ova-song-api/internal/startup"
 	descHealth "github.com/ozonva/ova-song-api/pkg/health-probe"
 	desc "github.com/ozonva/ova-song-api/pkg/ova-song-api"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 )
 
-const (
-	port = ":50051"
-)
-
 func main() {
-	tracerCloser, err := startup.InitJaegerTracer("ova-song-api", "127.0.0.1", ":6831")
+	conf := ReadConfig()
+
+	tracerCloser, err := startup.InitJaegerTracer(conf.Jaeger.ServiceName, conf.Jaeger.Host, conf.Jaeger.Port)
 	if err != nil {
 		log.Error().Err(err).Msg("Could not initialize jaeger tracer")
 	} else {
@@ -35,13 +35,13 @@ func main() {
 	}
 
 	go func() {
-		http.Handle("/metrics", promhttp.Handler())
-		if err := http.ListenAndServe(":8056", nil); err != nil {
+		http.Handle(conf.Metrics.HandleName, promhttp.Handler())
+		if err := http.ListenAndServe(conf.Metrics.ListenPort, nil); err != nil {
 			log.Error().Err(err).Msg("Failed to start listen to metric requests")
 		}
 	}()
 
-	broker, err := br.NewKafkaBroker([]string{"kafka:9092"}, "songs")
+	broker, err := br.NewKafkaBroker(conf.Kafka.Brokers, conf.Kafka.TopicName)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to start kafka producer")
 		broker = br.NewNullBroker() // graceful
@@ -53,22 +53,40 @@ func main() {
 		}()
 	}
 
-	lis, err := net.Listen("tcp", port)
+	lis, err := net.Listen("tcp", conf.Grpc.Port)
 	if err != nil {
-		log.Fatal().Err(err).Msgf("failed to listen: %v", err)
+		log.Fatal().Err(err).Msgf("failed to listen grpc port")
 	}
 
 	s := grpc.NewServer()
-	desc.RegisterOvaSongApiServer(s, createSongApi(broker))
 	descHealth.RegisterHealthServer(s, api.NewHealthApi())
+	desc.RegisterOvaSongApiServer(s, createSongApi(conf.Database, broker))
 	if err := s.Serve(lis); err != nil {
-		log.Fatal().Msgf("failed to serve: %v", err)
+		log.Fatal().Err(err).Msg("api failed to serve")
 	}
-	log.Fatal().Msgf("failed to serve: %v", err)
+	log.Fatal().Msgf("api failed to serve")
 }
 
-func createSongApi(broker br.Broker) desc.OvaSongApiServer {
-	dsn := "postgres://ova_song_db_user:ova_song_db_user@localhost:5432/ova_song_db?sslmode=disable"
+func ReadConfig() configuration.App {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal().Err(err).Msg("Error reading config file")
+	}
+
+	var conf configuration.App
+	err := viper.Unmarshal(&conf)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Unable to decode configuration into struct")
+	}
+
+	return conf
+}
+
+func createSongApi(config configuration.Database, broker br.Broker) desc.OvaSongApiServer {
+	dsn := "postgres://" + config.Username + ":" + config.Password + "@" +
+		config.Host + ":" + config.Port + "/" + config.DbName + "?sslmode=disable"
 
 	db, err := sqlx.Open("pgx", dsn)
 	if err != nil {
